2eb56f2fb4585dd51cf9ec1b947e699dfa8da389
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2024-12-26 12:22:10 +0100

    desktop responsiveness

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,10 +7,14 @@ import Logo from "@/assets/images/logo.svg";
 import { useEffect, useState } from "react";
 
 import { MdOutlineSegment } from "react-icons/md";
+import { Drawer } from "antd";
+
+import { FaTimes } from "react-icons/fa";
 
 const Navigation = () => {
 
     const [fixedNav, setFixed] = useState(false);
+    const [displayNavDrawer, setDisplayNavDrawer] = useState(false);
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -22,7 +26,9 @@ const Navigation = () => {
                 setFixed(false);
             }
         })
-    }, [])
+    }, []);
+
+    const toggleNavDrawer = () => setDisplayNavDrawer(!displayNavDrawer);
 
     return (
         <div className={`flex justify-between items-center py-7 px-5 md:px-20 z-50 fixed top-0 w-full ${fixedNav ? 'bg-white' : ''}`}>
@@ -59,9 +65,46 @@ const Navigation = () => {
             <div className="hidden md:block">
                 <Link className="text-accent border-2 border-solid border-accent px-8 py-3 rounded-xl" href="">Join Now</Link>
             </div>
-            <div className="block md:hidden">
+            <div onClick={toggleNavDrawer} className="block md:hidden">
                 <MdOutlineSegment className="text-3xl" />
             </div>
+            <Drawer open={displayNavDrawer} onClose={toggleNavDrawer}>
+                <div>
+                    <div className="border-b-2 border-solid border-[#F2F4F7] flex justify-between items-center p-5">
+                        <div>
+                            <Link href="/">
+                                <Image src={Logo} className="w-[80%] md:w-full" alt="logo" />
+                            </Link>
+                        </div>
+                        <div onClick={toggleNavDrawer}>
+                            <FaTimes className="text-xl" />
+                        </div>
+                    </div>
+                    <ul className="flex flex-col gap-5 py-10">
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">Home</Link>
+                        </li>
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">Blog</Link>
+                        </li>
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">Job Board</Link>
+                        </li>
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">About</Link>
+                        </li>
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">Services</Link>
+                        </li>
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">Events</Link>
+                        </li>
+                        <li className="border-b border-solid border-[#F2F4F7] px-5 pb-2">
+                            <Link className="text-sm font-dmsans" href="/">Join Now</Link>
+                        </li>
+                    </ul>
+                </div>
+            </Drawer>
         </div>
     )
 }
